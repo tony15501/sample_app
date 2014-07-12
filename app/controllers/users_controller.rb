0c6a76974c8caca2bc4a34e4eed6f5a1b7bc87da
tony15501 <your.marco.antonio.medina81@gmail.com>
2014-07-11 19:24:00 -0500

    add form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
-  def new
+ def show
+    @user = User.find(params[:id])
   end
+
+  def new
+  	 @user = User.new
+  end
+  def create
+  	if @user.save
+  		
+  	else
+  		render 'new'
+  	end
+  end
+  private
+  	 def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  resources :users
   get "users/new"
   match '/signup',  to: 'users#new', via: 'get'
   #get "static_pages/home"
