c6770bc7966344b67a7d146d9e9eb2ed84a56fd0
tony15501 <chekoferreiro@gmail.com>
2014-07-18 00:31:14 -0500

    add test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,13 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate)}
   it { should respond_to(:admin)}
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
   
   it {should be_valid}
   it {should_not be_admin}
@@ -66,6 +73,29 @@ describe "with a password that's too short" do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+      describe "and unfollowing" do
+        before { @user.unfollow!(other_user) }
+
+        it { should_not be_following(other_user) }
+        its(:followed_users) { should_not include(other_user) }
+      end
   end
 end
   
